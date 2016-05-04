c74df9d432248b4afc391274c610297f45f05bc1
Charlie Proctor <charlie@charlieproctor.com>
2016-05-04 19:51:04 -0400

    metric = callable

--- kneighbors.py
+++ kneighbors.py
@@ -3,7 +3,7 @@ from metric import distance
 
 class KNeighbors(object):
     def __init__(self):
-        self.kn = NearestNeighbors(n_neighbors=100,metric='pyfunc',func=distance)
+        self.kn = NearestNeighbors(n_neighbors=3,metric=distance)
 
     def fit(self,X):
         return self.kn.fit(X)
