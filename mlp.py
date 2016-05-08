23de28de098cf9bc4a46ef6665460122146969c2
Charlie Proctor <charlie@charlieproctor.com>
2016-05-08 02:58:09 -0400

    parse and load model file command line options

--- mlp.py
+++ mlp.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-import sys,json
+import sys,json,os
 from marble import Marble
 from optparse import OptionParser
 
@@ -33,7 +33,7 @@ class MLPMarble(Marble):
 
 if __name__ == '__main__':
 
-    parser = OptionParser()
+    parser = OptionParser(usage="usage: mlp.py <train|test> [options]")
     parser.add_option("-l", "--lyrics", dest="lyrics_root",
                       help="where are the lyrics files located?", default="lyrics/")
     parser.add_option("-v", "--verbose",
@@ -43,13 +43,42 @@ if __name__ == '__main__':
                       help="number of artists to run")
     parser.add_option("-c", "--conf", dest="conf", default="conf/mlp.json",
             help="location of the mlp json config file, specifying model parameters")
+    parser.add_option("-f", "--filename", dest="model_name", default="out/model.mlp",
+            help="location of the model file (either to write if training, or to read if testing)")
 
     (options, args) = parser.parse_args()
 
+    if len(sys.argv) < 2:
+        parser.print_help()
+        sys.exit(1)
+    else:
+        # train | test
+        mode = sys.argv[1]
+
+        # make sure it's either train | test
+        if mode != "train" and mode != "test":
+            parser.print_help()
+            sys.exit(1)
+
+        sys.stderr.write("mode = " + mode + "\n")
+    
     # open and parse the config file
     with open(options.conf) as f:
         conf = json.load(f)
         sys.stderr.write("Using parameters: " + str(conf) + "\n")
 
+    # make the training directory if it does not already exist
+    if mode == "train" and not os.path.isdir(os.path.dirname(options.model_name)):
+        os.mkdir(os.path.dirname(options.model_name))
+
+    # open the model file
+    with open(options.model_name,("w" if mode == "train" else "r")) as model_file:
+        sys.stderr.write("Model file: " + options.model_name + "\n")
+
         d = MLPMarble(options.lyrics_root,conf,verbose=options.verbose,max_artists=options.max_artists)
-        d.train()
+        
+        if mode == "train":
+            d.train(model_file)
+        else:
+            d.test(model_file)
+
