267ecb2c1467d1aebf299853aaae052bc02c2351
Charlie Proctor <charlie@charlieproctor.com>
2016-05-09 05:05:46 -0400

    comments in split.py

--- util/split.py
+++ util/split.py
@@ -4,20 +4,28 @@ import sys,os,random
 from optparse import OptionParser
 
 def split(lyrics_root, test_root, test_size=0.4):
+    """
+    Split the artists in lyrics_root into a test data set of size test_size * len(artists)
+    """
 
+    # calculate the paths
     paths = [path for path in os.walk(lyrics_root)]
 
+    # grab a random sample of paths
     test = random.sample(paths,int(round(test_size*float(len(paths)))))
 
+    # iterate through all the new test lyrics
     for (dirpath, dirnames, filenames) in test:
         if dirpath == lyrics_root:
             continue
 
+        # calculate destination
         src = dirpath 
         dest = os.path.join(test_root, os.path.basename(dirpath))
 
         print src, "->", dest
 
+        # and rename to move the artist!
         os.rename(src,dest)
 
 if __name__ == "__main__":
